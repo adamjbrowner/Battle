08ab6a5a1eeb2c59dd7ad176702fcffc86a757b1
benjamin-white <benjaminwhite@live.co.uk>
2016-07-20 13:02:05 +0100

    DRY tests with web_helpers.rb

--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature "Hitpoints" do
   scenario "viewing hitpoints" do
-    visit ('/')
-    fill_in :player_1_name, with: 'Adam'
-    fill_in :player_2_name, with: 'Ben'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ben: 60HP'
   end
 end
