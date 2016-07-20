6273c8bbb10bf798844801d8c548349f09d64f16
Adam <adamjbrowner@gmail.com>
2016-07-20 13:17:38 +0100

    Added attack button: challenge 22

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,3 +6,18 @@ feature "enter names" do
   expect(page).to have_content 'Adam vs. Ben'
   end
 end
+
+feature "Hitpoints" do
+  scenario "viewing hitpoints" do
+    sign_in_and_play
+    expect(page).to have_content 'Ben: 60HP'
+  end
+end
+
+feature "Attack" do
+  scenario "Attacking with confirmation" do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Adam attacked Ben'
+  end
+end
--- spec/features/hitpoints_spec.rb
+++
@@ -1,6 +0,0 @@
-feature "Hitpoints" do
-  scenario "viewing hitpoints" do
-    sign_in_and_play
-    expect(page).to have_content 'Ben: 60HP'
-  end
-end
